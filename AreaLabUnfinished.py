ca947e2d1328ad22b32bc6edc17baae8f6fea1eb
Alex <alex.alyakrinsky@gmail.com>
2018-09-25 11:03:22 -0700

    finished

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -8,7 +8,7 @@ def rectArea(base, height):
     return base * height
 
 def trapArea(base1, base2, height):
-    return height * abs( (base1 + base2) / 2)
+    return height * (base1 + base2) / 2
 
 def triArea(base,height):
     return (base * height)/2
@@ -17,10 +17,14 @@ def triArea(base,height):
 class MyTest(unittest.TestCase): #using TestCase class from unittest modual
     def testCircleArea(self):
         self.assertEqual(circleArea(5), 25 * math.pi)
+        self.assertAlmostEqual(circleArea(3.25), 33.1830724035)
     def testRectArea(self):
         self.assertEqual(rectArea(3, 10), 30)
+        self.assertEqual(rectArea(5, 12), 60)
     def testTrapArea(self):
         self.assertEqual(trapArea(14, 20, 12), 204)
+        self.assertEqual(trapArea(10, 20, 2), 30)
     def testTriArea(self):
         self.assertEqual(triArea(6, 6), 18)
+        self.assertEqual(triArea(10, 10), 50)
     
